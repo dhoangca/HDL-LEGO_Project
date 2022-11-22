a903a2a63ae04c357ad22ce046e3087d3f638755
Ngduc2803 <taotenduc28@gmail.com>
2022-11-22 10:49:15 +0700

    create product model

---
+++ app/Models/Back_end/ProductModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Back_end;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model
+{
+    use HasFactory;
+}
