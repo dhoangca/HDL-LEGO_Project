44d7691a5c78ab213a33e1a5748c0b976ad0e7be
dhoangca <112866613+dhoangca@users.noreply.github.com>
2022-12-06 17:54:46 +0700

    Update ProductModel.php

--- app/Models/Back_end/ProductModel.php
+++ app/Models/Back_end/ProductModel.php
@@ -4,8 +4,16 @@ namespace App\Models\Back_end;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class ProductModel extends Model
 {
     use HasFactory;
+    public $table ="product";
+    public $primaryKey = 'pID';
+    public $fillable = 
+    [
+        'pName', 'pDescription','pImage','pPrice','pPricePro','pQuantity','FeaturedRecent'
+    ];
+    public $timestamps = false;
 }
